b27e0e8eeea4af49dec019c28eb15674acbddd2c
Sergio Calderón <66924320+sergiocarp10@users.noreply.github.com>
2022-12-31 17:02:24 -0300

    practicando con textos

--- app/src/main/java/cs10/apps/jetpackcomposeapp/MainActivity.kt
+++ app/src/main/java/cs10/apps/jetpackcomposeapp/MainActivity.kt
@@ -3,13 +3,18 @@ package cs10.apps.jetpackcomposeapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import cs10.apps.jetpackcomposeapp.ui.theme.JetpackComposeAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,6 +37,30 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String) {
     Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TitleText(){
+    Column(modifier = Modifier.fillMaxWidth()) {
+        Text(
+            text = "Jue 21/4 - 15:20 / 16:50",
+            fontWeight = FontWeight.Bold,
+            fontSize = 24.sp
+        )
+
+        Text(
+            text = "Av. 1 y 53 - Cruce Varela",
+            fontSize = 18.sp
+        )
+
+        Text(
+            text = "Centenario ($32,09)",
+            fontSize = 18.sp
+        )
+    }
+
+
 }
 
 @Preview(showBackground = true)
