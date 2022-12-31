5b90e6956a266b8b51e144868cc7e27a3ff23503
Sergio Calderón <66924320+sergiocarp10@users.noreply.github.com>
2022-12-31 18:31:40 -0300

    practicando con iconos y layouts

--- app/src/main/java/cs10/apps/jetpackcomposeapp/MainActivity.kt
+++ app/src/main/java/cs10/apps/jetpackcomposeapp/MainActivity.kt
@@ -3,14 +3,17 @@ package cs10.apps.jetpackcomposeapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -41,29 +44,50 @@ fun Greeting(name: String) {
 
 @Preview(showBackground = true)
 @Composable
-fun TitleText(){
-    Column(modifier = Modifier.fillMaxWidth()) {
-        Text(
-            text = "Jue 21/4 - 15:20 / 16:50",
-            fontWeight = FontWeight.Bold,
-            fontSize = 24.sp
+fun TravelItem() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(color = Color.Blue)
+    ){
+        Icon(
+            painter = painterResource(id = R.drawable.ic_bus),
+            contentDescription = null,
+            tint = Color.White,
+            modifier = Modifier
+                .align(Alignment.CenterVertically)
+                .size(70.dp)
+                .padding(horizontal = 8.dp)
         )
 
-        Text(
-            text = "Av. 1 y 53 - Cruce Varela",
-            fontSize = 18.sp
-        )
+        Column {
+            Text(
+                text = "Jue 21/4 - 15:20 / 16:50",
+                fontWeight = FontWeight.Bold,
+                fontSize = 24.sp,
+                color = Color.White
+            )
 
-        Text(
-            text = "Centenario ($32,09)",
-            fontSize = 18.sp
-        )
+            Text(
+                text = "Av. 1 y 53 - Cruce Varela",
+                fontSize = 18.sp,
+                color = Color.White
+            )
+
+            Text(
+                text = "Centenario ($32,09)",
+                fontSize = 18.sp,
+                color = Color.White
+            )
+        }
     }
+
+
 
 
 }
 
-@Preview(showBackground = true)
+// @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     JetpackComposeAppTheme {
